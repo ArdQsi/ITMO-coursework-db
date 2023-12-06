529d8e807d88a003f0bb0fa434819dd678a4ef9c
Roman <roman_kravets_den@mail.ru>
2023-12-07 01:27:18 +0300

    front work

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route, Link} from 'react-router-dom'
 import MainPage from "./pages/MainPage";
 import ProcessorsPage from "./pages/ProcessorsPage";
+import CasesPage from "./pages/CasesPage";
 
 
 function App() {
@@ -9,11 +10,19 @@ function App() {
         <>
             <header>
                 <Link to="/">Main</Link>
+                <Link to="/cases">ComputerCases</Link>
+                <Link to="/datastorage">DataStorage</Link>
+                <Link to="/graphicscards">GraphicCards</Link>
+                <Link to="/motherboards">MotherBoards</Link>
+                <Link to="/powersupply">PowerSupply</Link>
                 <Link to="/processors">Processors</Link>
+                <Link to="/rammemory">RamMemory</Link>
+
             </header>
             <Routes>
                 <Route path="/" element={<MainPage />}/>
                 <Route path="/processors" element={<ProcessorsPage />}/>
+                <Route path="/cases" element={<CasesPage />}/>
             </Routes>
         </>
 
---
+++ front/src/pages/CasesPage.js
@@ -0,0 +1,10 @@
+const CasesPage = () => {
+
+    return (
+        <div>
+            cases
+        </div>
+    );
+};
+
+export default CasesPage;
