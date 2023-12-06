7c2e593c23ba57158d419799cf70a5a57cb75e6d
Denstep <denvvbaranov@gmail.com>
2023-12-06 04:57:39 +0300

    add fun

--- back/src/main/java/com/example/coursework/сontrollers/MainController.java
+++ back/src/main/java/com/example/coursework/сontrollers/MainController.java
@@ -3,11 +3,14 @@ package com.example.coursework.сontrollers;
 import com.example.coursework.database.Result;
 import com.example.coursework.service.ResultService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,7 @@ public class MainController {
     final private ResultService resultService;
 
     @GetMapping("/main")
-    public List<Result> main(@RequestParam(value = "price", defaultValue = "0") int price) {
+    public List<Result> main(@RequestParam(value = "price", defaultValue = "30000") int price) {
         List<Result> list = resultService.getResult(price);
         return list;
     }
