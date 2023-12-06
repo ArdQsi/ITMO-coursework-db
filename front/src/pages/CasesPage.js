7cb3a6df4c2a9b6118835e28e8560b45b2680455
Roman <roman_kravets_den@mail.ru>
2023-12-07 02:01:39 +0300

    front

--- front/src/pages/CasesPage.js
+++ front/src/pages/CasesPage.js
@@ -1,8 +1,77 @@
+import {useEffect, useState} from "react";
+import DataTable from "react-data-table-component";
+
 const CasesPage = () => {
+    const [price, setPrice] = useState('0');
+    const [data, setData] = useState('');
+
+    const handleChange = (event) => {
+        setPrice(event.target.value);
+    };
+
+    useEffect(() => {
+        fetch('http://localhost:8080/computer-cases', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((response) => setData(response))
+    }, [])
+
+
+    const handleClick = (param) => {
+        const newPost = {
+            "price": param
+        }
+
+        fetch('http://localhost:8080/computer-cases', {
+            method: 'POST',
+            body: JSON.stringify(newPost),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((response) => console.log(response))
+            // .then(response => setData(response))
+    }
+    const columns = [
+        {
+            name: "name",
+            selector: row => row.name
+        },
+        {
+            name: "manufacturer",
+            selector: row => row.manufacturer
+        },
+        {
+            name: "color",
+            selector: row => row.color
+        },
+        {
+            name: "formFactor",
+            selector: row => row.formFactor
+        },
+        {
+            name: "price",
+            selector: row => row.price
+        }
+    ]
 
     return (
         <div>
-            cases
+            <input
+                type="text"
+                id="price"
+                name="price"
+                onChange={handleChange}
+                value={price}
+            />
+            <button onClick={() => handleClick(price)}>send</button>
+            <DataTable columns={columns} data={data} fixedHeader pagination>
+            </DataTable>
         </div>
     );
 };
